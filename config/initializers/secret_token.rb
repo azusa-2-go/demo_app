798c9fc59a06d4e9600f836cfb4cdfc291c7d866
azusa-2-go <azusa.2.go.to.theworld@gmail.com>
2014-09-02 16:26:40 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '519aa42865e17c35f6827b5b75bde3864ec9377cfbf05f807482e713fec82a1e9bdc946afc049a884bf6ee7a993818888d234391bb3b23d0540b1920889f29a0'
